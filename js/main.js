5df887c8df8d15f7a80c217b7f3414c63d996962
Jasurbek <jaskasamadov695@gmail.com>
2023-12-21 13:58:11 +0500

    masdfddh

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", function () {
+      const accordionItem = this.parentElement;
+      accordionItem.classList.toggle("active");
+
+      const accordionContent =
+        accordionItem.querySelector(".accordion-content");
+      if (accordionItem.classList.contains("active")) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      } else {
+        accordionContent.style.maxHeight = 0;
+      }
+
+      // Close other accordion items
+      const otherAccordionItems = document.querySelectorAll(
+        ".accordion-item:not(.active)"
+      );
+      otherAccordionItems.forEach((item) => {
+        item.classList.remove("active");
+        item.querySelector(".accordion-content").style.maxHeight = 0;
+      });
+    });
+  });
+});
